f9dc58d4757c52e190ce4981e72ffdfa754a494f
thoma4kw <thoma4kw@mail.uc.edu>
2022-09-25 20:46:18 -0400

    done

--- exercises/password-reveal/app.js
+++ exercises/password-reveal/app.js
@@ -1,12 +1,24 @@
-// 🤖: create 2 constants with references to the password input and the reveal button from the DOM
-
-
-// 🤖: Create an Event Listener on the password reveal button
-// 🤖: The event listener should toggle the password field attribute between `password` and `text` to change the visibility of the password field content.
-
-
-// Provided for you - prevent form submit
+// Existing app.js code.
 const form = document.querySelector("form");
-form.addEventListener('submit',function(e){
-	e.preventDefault();
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+});
+
+// Getting reference for input field and toggle button
+// by IDs.
+const input = document.getElementById("password");
+const togglePassword = document.getElementById("togglePassword");
+
+// Adding event listner to reveal password.
+togglePassword.addEventListener("click", function (e) {
+  // This function will check if input type is text
+  // if this is the case then it will change inputy type
+  // to password from text and vice versa.
+  if (input.type === "text") {
+    // input type is text
+    input.type = "password"; // change type to password
+  } else {
+    // input type is already password
+    input.type = "text"; // change type to text.
+  }
 });
--- exercises/word-counter/js/app.js
+++ exercises/word-counter/js/app.js
@@ -1,6 +1,26 @@
-// 🤖: create 2 constants with references to textarea input and the stats section from the DOM
 
 
-// 🤖: Create an Event Listener on textarea input
-// 🤖: The event handler should update the stats section with the number of words and characters in the textarea input.
-// 🤖: You will need to utilize the split method to get the words count.
+// Getting reference of textarea using query selector
+// method since there is no ids associated for this textarea.
+const textarea = document.querySelector("textarea");
+// Getting refrence of stat for showing words and characters count
+const stat = document.getElementById("stat");
+// add onkeyup event on textarea this event will fire on every key up.
+textarea.addEventListener("onkeyup", function (e) {
+    // initialize words and chars variable to hold word and chars count.
+  let words = 0;
+  let chars = 0;
+  if (textarea.value) { // check if there is something written in textarea.
+    // get word count, textarea.value return value written in textarea
+    // trim function removes all trailing and leading spaces.
+    // split function splits string into array of word and calling length on 
+    // result of split will give us total words.
+    words = textarea.value.trim().split(" ").length;
+    // to get chars count simpliy trim input value first then 
+    // access length property to get number of characters.
+    chars = textarea.value.trim().length;
+  }
+  // set stat with textarea words and characters count.
+  stat.innerHTML =
+    "You've written " + words + " words and " + chars + " characters.";
+});
